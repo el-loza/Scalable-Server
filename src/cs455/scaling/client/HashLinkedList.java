e1ea551ee76126cb9b5fc77be0c74de0772afd96
el-loza <ethan.loza@gmail.com>
2017-03-06 13:22:36 -0700

    Made intial changes

---
+++ src/cs455/scaling/client/HashLinkedList.java
@@ -0,0 +1,7 @@
+package cs455.scaling.client;
+
+/**
+ * Created by eloza on 3/5/17.
+ */
+public class HashLinkedList {
+}
---
+++ src/cs455/scaling/utilities/ByteGenerator.java
@@ -0,0 +1,40 @@
+package cs455.scaling.utilities;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by eloza on 3/4/17.
+ */
+public class ByteGenerator {
+
+    public static byte[] randomMessage(int messSize){
+        byte[] byteArr = new byte[messSize];
+        new Random().nextBytes(byteArr);
+        return byteArr;
+    }
+
+    public static String SHA1FromBytes(byte[] data) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA1");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        byte[] hash = digest.digest(data);
+        BigInteger hashInt = new BigInteger(1, hash);
+        return hashInt.toString(16);
+    }
+
+    public static void main(String[] args){
+        byte[] rando = ByteGenerator.randomMessage(8000);
+        String hashers = ByteGenerator.SHA1FromBytes(rando);
+        String randoString = new String(rando);
+        System.out.println(randoString);
+        System.out.println(hashers);
+    }
+}
+
